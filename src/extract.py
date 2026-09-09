c391903f21c3c5ae9f0a70a8b677351f6c6e37d7
Mohammad Ikbal <mohammadikbal2006@gmail.com>
2026-09-09 10:49:54 -0400

    Added extract.py file

---
+++ src/extract.py
@@ -0,0 +1,69 @@
+import requests
+import os
+import pandas as pd
+from dotenv import load_dotenv
+
+print("Extracting Hearthstone cards...")
+def extract_hearthstone_cards(): 
+    load_dotenv()
+    client_id = os.getenv("BLIZZARD_CLIENT_ID")
+    client_secret = os.getenv("BLIZZARD_CLIENT_SECRET")
+
+    # Get access token
+    token_response = requests.post(
+        "https://oauth.battle.net/token",
+        data={"grant_type": "client_credentials"},
+        auth=(client_id, client_secret)
+    )
+
+    token_response.raise_for_status()
+    access_token = token_response.json()["access_token"]
+
+    print("Token received.")
+
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    # Fetch all Hearthstone cards
+    all_cards = []
+    page = 1
+
+    cards_url = "https://us.api.blizzard.com/hearthstone/cards"
+
+    while True:
+        response = requests.get(
+            cards_url,
+            headers=headers,
+            params={
+                "locale": "en_US",
+                "pageSize": 100,
+                "page": page
+            }
+        )
+
+        response.raise_for_status()
+        data = response.json()
+
+        cards = data.get("cards", [])
+
+        if not cards:
+            break
+
+        all_cards.extend(cards)
+
+        print(f"Page {page} fetched — {len(all_cards)} cards total")
+
+        if page >= data["pageCount"]:
+            break
+
+        page += 1
+
+    hearthstone_df = pd.DataFrame(all_cards)
+    hearthstone_df.to_csv("../bronze_hearthstone_data/hearthstone_cards.csv", index=False)
+    print("Data saved to CSV.")
+    return hearthstone_df 
+
+
+df = extract_hearthstone_cards()
+print(df.head())
